7a7243d8ee1452297489c685a9b72cd58cd70844
DANIEL DA SILVA PEREIRA <60305462+danielpereira-gub@users.noreply.github.com>
2021-04-21 17:34:43 -0300

    Update instalador.sh

--- INSTALADORES/instalador.sh
+++ INSTALADORES/instalador.sh
@@ -3,7 +3,7 @@
 ##MACROS###
 
 URL_ZABBIX=https://repo.zabbix.com/zabbix/5.3/rhel/8/x86_64/zabbix-release-5.3-1.el8.noarch.rpm
-URL_GRAFANA=https://dl.grafana.com/oss/release/grafana-7.4.3-1.x86_64.rpm
+URL_GRAFANA=https://dl.grafana.com/oss/release/grafana-7.5.4-1.x86_64.rpm
 
 ###INICIAL###
 TIME=1
